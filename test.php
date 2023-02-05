087d53e37b5bd8b2f3d2016fe33ca1c6e29eeebf
blackdavinci <ousmaneciss1@gmail.com>
2023-02-04 20:55:10 -0500

    Update

--- test.php
+++ test.php
@@ -5,8 +5,8 @@ require_once 'vendor/autoload.php';
 use \NimbaSMS\NimbaSMS;
 
 $config = [
-	'serviceId' => 'ba270d0c088652ac7ba3b58fa6ee8c8b',
-	'secretToken' => 'LP-3lv5Q-GV4zp6YK31n-DUhYQ0FO5CCThZ5r2NmX37HnY3TUqxIhoa10innIJ4Aws0aW86Erd6ovMsYxQVEQPaNcnHwQBcU3xLsCFleqFw'
+	'serviceId' => 'XXXXXXXXXXXXXXXXXXXXXXXX',
+	'secretToken' => 'XXXXXXXXXXXXXXXXXXXXXXXX'
 ];
 
 // or 
